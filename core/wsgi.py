a2073db364f490a5ff0504579b9908c5d871f4fc
vasylhnatiuk <vasylhnatiuk1@gmail.com>
2026-04-23 10:49:39 -0400

    update wsgi

--- core/wsgi.py
+++ core/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+path = '/home/vasylhnatiuk/photographer'
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
