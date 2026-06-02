854febe666e94af98c2ef54241f99cc0fee00650
vasylhnatiuk <vasylhnatiuk1@gmail.com>
2026-06-02 14:10:06 -0400

    update

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path("weddings/", views.weddings, name="weddings"),
+    path("love_stories/", views.weddings, name="weddings"),
     path("portraits/", views.portraits, name="portraits"),
     path("events/", views.events, name="events"),
     path('albums/', views.albums, name='albums'),
